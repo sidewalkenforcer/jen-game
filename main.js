f41b90eae420d48a89554cc064b0754ab27f3387
missionbit <missionbit@missionbits-MacBook-Air-6.local>
2018-07-16 11:45:21 -0700

    Assests folder

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', { preload: preload
 
 function preload() {
 
-    game.load.image('sky', 'assets/sky.png');
+    game.load.image('blue car', 'assets/pixel art car 1 (1).png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
@@ -11,7 +11,7 @@ function preload() {
 
 function create() {
 
-	game.add.sprite(0, 0, 'star');
+	game.add.sprite(100, 100, 'blue car');
 
 }
 
