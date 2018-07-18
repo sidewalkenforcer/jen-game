2ea4ece0de76cb7aa69c3e52c7a44aeb05f5c0c6
sidewalkenforcer <nwaterford1@gmail.com>
2018-07-18 10:05:33 -0700

    hh

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', {
+    preload: preload,
+    create: create,
+    update: update
+});
 
 function preload() {
+    game.load.image('red car',
+        'assets/pixel art car 1 (2).png')
 
     game.load.image('blue car', 'assets/pixel art car 1 (1).png');
-    game.load.image('ground', 'assets/platform.png');
+    game.load.image('battery', 'assets/battery power up.gif');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
@@ -11,9 +17,11 @@ function preload() {
 
 function create() {
 
-	game.add.sprite(100, 100, 'blue car');
+    game.add.sprite(100, 100, 'blue car');
+    game.add.sprite(200, 200,
+        'red car');
+    game.add.sprite(150, 300, 'battery');
 
 }
 
-function update() {
-}
+function update() {}
