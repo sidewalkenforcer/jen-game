9bd9cd0a1648a54fce4b5826b7ccf258d9fe469a
sidewalkenforcer <nwaterford1@gmail.com>
2018-07-19 09:59:45 -0700

    adding

--- main.js
+++ main.js
@@ -5,82 +5,78 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', {
 });
 
 function preload() {
-
-	game.load.spritesheet('redcar', 'assets/pixel art car 1 version 2.0.png', 60, 25);
+    game.load.image('battery up',
+        'assets/battery up 2.png');
+    game.load.spritesheet('freexe power',
+        'assets/freexe power up.png', 150, 200);
+    game.load.spritesheet('redcar', 'assets/pixel art car 1 version 2.0.png', 60, 25);
     game.load.spritesheet('bluecar', 'assets/pixel art car 1 (1).png', 60, 25);
 
 }
-	var keys;
+var keys;
 
 
 function create() {
-	
-	this.redcar = this.game.add.sprite(180,225, 'redcar')
+    game.add.sprite(50, 100, 'battery up');
+    game.add.sprite(20, 20, 'freexe power');
 
-	this.redcar.anchor.set(0.5);
-	
-	game.physics.arcade.enable(this.redcar);
-	
-	
-	this.cursor = {
-		up: this.game.input.keyboard.addKey(Phaser.Keyboard.UP),
-		down: this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
-		left: this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-		right: this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-	};
-	
-	
-	
-	this.bluecar = this.game.add.sprite(90,225, 'bluecar')
+    this.redcar = this.game.add.sprite(180, 225, 'redcar')
+
+    this.redcar.anchor.set(0.5);
+
+    game.physics.arcade.enable(this.redcar);
 
 
-	this.bluecar.anchor.set(0.5);
-	
-	game.physics.arcade.enable(this.bluecar);
-	
-	this.bluecarControls = {
-		up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
-		down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
-		left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
-		right: this.game.input.keyboard.addKey(Phaser.Keyboard.D)
-	};
-	
-	
+    this.cursor = {
+        up: this.game.input.keyboard.addKey(Phaser.Keyboard.UP),
+        down: this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
+        left: this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+        right: this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+    };
+
+
+
+    this.bluecar = this.game.add.sprite(90, 225, 'bluecar')
+
+
+    this.bluecar.anchor.set(0.5);
+
+    game.physics.arcade.enable(this.bluecar);
+
+    this.bluecarControls = {
+        up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+        down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+        left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+        right: this.game.input.keyboard.addKey(Phaser.Keyboard.D)
+    };
+
+
 }
 
 function update() {
-	this.redcar.body.velocity.x = 0;
-	this.redcar.body.velocity.y = 0;
-	this.redcar.body.angularVelocity = 0;
-	
-	if (this.cursor.left.isDown)
-		{
-			this.redcar.body.angularVelocity = -250;
-		}
-	else if (this.cursor.right.isDown)
-		{
-			this.redcar.body.angularVelocity = 250;
-		}
-	if (this.cursor.up.isDown)
-		{
-			this.game.physics.arcade.velocityFromAngle(this.redcar.angle, 250, this.redcar.body.velocity);
-		}
+    this.redcar.body.velocity.x = 0;
+    this.redcar.body.velocity.y = 0;
+    this.redcar.body.angularVelocity = 0;
 
-	this.bluecar.body.velocity.x = 0;
-	this.bluecar.body.velocity.y = 0;
-	this.bluecar.body.angularVelocity = 0;
-	
-	if (this.bluecarControls.left.isDown)
-		{
-			this.bluecar.body.angularVelocity = -250;
-		}
-	else if (this.bluecarControls.right.isDown)
-		{
-			this.bluecar.body.angularVelocity = 250;
-		}
-	if (this.bluecarControls.up.isDown)
-		{
-			this.game.physics.arcade.velocityFromAngle(this.bluecar.angle, 250, this.bluecar.body.velocity);
-		}
+    if (this.cursor.left.isDown) {
+        this.redcar.body.angularVelocity = -250;
+    } else if (this.cursor.right.isDown) {
+        this.redcar.body.angularVelocity = 250;
+    }
+    if (this.cursor.up.isDown) {
+        this.game.physics.arcade.velocityFromAngle(this.redcar.angle, 250, this.redcar.body.velocity);
+    }
+
+    this.bluecar.body.velocity.x = 0;
+    this.bluecar.body.velocity.y = 0;
+    this.bluecar.body.angularVelocity = 0;
+
+    if (this.bluecarControls.left.isDown) {
+        this.bluecar.body.angularVelocity = -250;
+    } else if (this.bluecarControls.right.isDown) {
+        this.bluecar.body.angularVelocity = 250;
+    }
+    if (this.bluecarControls.up.isDown) {
+        this.game.physics.arcade.velocityFromAngle(this.bluecar.angle, 250, this.bluecar.body.velocity);
+    }
 }
-
