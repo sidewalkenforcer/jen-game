87402d6435279d8f3fed3042542bfec92b3f0f6d
missionbit <missionbit@missionbits-MacBook-Air-6.local>
2018-07-19 12:56:30 -0700

    added chicken, animation

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', {
 
 function preload() {
 
-	game.load.spritesheet('redcar', 'assets/pixel art car 1 version 2.0.png', 60, 25);
+	game.load.spritesheet('redcar', 'assets/pixel art car 1 (2).png', 60, 25);
     game.load.spritesheet('bluecar', 'assets/pixel art car 1 (1).png', 60, 25);
+	game.load.image('chicken', 'assets/New Piskel.png', 32, 32);
 
 }
 	var keys;
@@ -15,20 +16,22 @@ function preload() {
 
 function create() {
 	
+	game.add.sprite(100, 100, 'chicken');
+	
 	this.redcar = this.game.add.sprite(180,225, 'redcar')
 
 	this.redcar.anchor.set(0.5);
 	
 	game.physics.arcade.enable(this.redcar);
 	
+ 
 	
 	this.cursor = {
 		up: this.game.input.keyboard.addKey(Phaser.Keyboard.UP),
 		down: this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
 		left: this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
 		right: this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-	};
-	
+	}
 	
 	
 	this.bluecar = this.game.add.sprite(90,225, 'bluecar')
@@ -45,6 +48,8 @@ function create() {
 		right: this.game.input.keyboard.addKey(Phaser.Keyboard.D)
 	};
 	
+	this.redcar.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.bluecar.animations.add('right', [0, 1, 2, 3], 10, true);
 	
 }
 
@@ -56,10 +61,12 @@ function update() {
 	if (this.cursor.left.isDown)
 		{
 			this.redcar.body.angularVelocity = -250;
+			this.redcar.animations.play('right');
 		}
 	else if (this.cursor.right.isDown)
 		{
 			this.redcar.body.angularVelocity = 250;
+			this.redcar.animations.play('left');
 		}
 	if (this.cursor.up.isDown)
 		{
@@ -73,10 +80,12 @@ function update() {
 	if (this.bluecarControls.left.isDown)
 		{
 			this.bluecar.body.angularVelocity = -250;
+			this.bluecar.animations.play('right');
 		}
 	else if (this.bluecarControls.right.isDown)
 		{
 			this.bluecar.body.angularVelocity = 250;
+			this.bluecar.animations.play('left');
 		}
 	if (this.bluecarControls.up.isDown)
 		{
